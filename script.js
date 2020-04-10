696d733879412fb8ecd9fbfad92f1a87fd524636
RemyJS <a.king92@mail.ru>
2020-04-10 16:22:45 +0300

    feat:add card play

--- script.js
+++ script.js
@@ -1,11 +1,9 @@
 //import cards from './assets/cards'
 console.log(cards);
 const category = cards[0];
-const nav = document.querySelectorAll(".nav_item");
-
+const nav = document.querySelectorAll(".menu__item");
+const cardText = document.querySelectorAll(".card__text");
 for(let i = 0; i < category.length; i++){
     nav[i].text = category[i];
-}
-function hi (){
-    console.log("hi");
+    cardText[i].textContent = category[i];
 }
