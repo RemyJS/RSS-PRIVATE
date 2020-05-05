b2a39d4b6c5dc4e3c619e1767f377e5d9db44f98
RemyJS <a.king92@mail.ru>
2020-05-05 16:05:22 +0300

    feat: add yandex translation api

---
+++ src/scripts/translate.js
@@ -0,0 +1,16 @@
+console.log("translate init");
+async function getEnglishTitle(title) {
+
+  const key = "trnsl.1.1.20200503T203406Z.732ca858232efec6.abdb64d495644f843b7b88aed96ccdb7ffac5ac2";
+  const url = `https://translate.yandex.net/api/v1.5/tr.json/translate?key=${key}&text=${title}&lang=ru-en`;
+  const res = await fetch(url);
+  const data = await res.json();
+
+  return data;
+}
+
+// const promise = getEnglishTitle("Человек паук");
+
+// promise.then((value) =>{
+//   console.log(value.text[0]);
+// })
