da46840225378cb66299ae4947391b42c36389d2
RemyJS <a.king92@mail.ru>
2020-05-07 13:33:31 +0300

    feat: add clear button

--- src/scripts/search.js
+++ src/scripts/search.js
@@ -5,10 +5,10 @@ import { showMessage } from "./notification.js";
 function initSearch() {
   console.log("initSearh export");
   const searchButton = document.querySelector(".search__button");
-
+  const searchClear = document.querySelector(".search__clear");
+  const searchInput = document.querySelector(".search__input");
   searchButton.addEventListener("click", (event) => {
     event.preventDefault();
-    const searchInput = document.querySelector(".search__input");
     const title = searchInput.value;
     if (title.match(/[А-я]/g)) { // if title has russian letters
       const titleEng = getEnglishTitle(title);
@@ -21,6 +21,11 @@ function initSearch() {
     } else {
       initSlider(title);
     }
+  });
+
+  searchClear.addEventListener("click", () => {
+    searchInput.value = "";
+    // searchInput.focus();
   });
 }
 
