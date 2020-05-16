1ee7ddc461b7c0864a22e249ff1845daf93910ef
RemyJS <a.king92@mail.ru>
2020-05-16 12:36:20 +0300

    init: webpack

---
+++ src/scripts/app.js
@@ -0,0 +1,3 @@
+import '../styles/style.css';
+
+console.log("hello app.js");
---
+++ webpack.config.js
@@ -0,0 +1,41 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const { CleanWebpackPlugin } = require("clean-webpack-plugin");
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+
+module.exports = {
+  entry: './src/scripts/app.js',
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'bundle.js'
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template:"src/index.html"
+    }),
+    new CleanWebpackPlugin(),
+    new MiniCssExtractPlugin({
+      filename: 'style.css',
+    })
+  ],
+  module : {
+    rules : [
+      {
+        test: /\.css$/i,
+        use: [MiniCssExtractPlugin.loader, 'css-loader'],
+      },
+      {
+        test: /\.(png|svg|jpe?g|gif)$/,
+        use: {
+          loader: "file-loader",
+          options: {
+            outputPath: "assets/img",
+          },
+        },
+      },
+    ],
+  },
+  devServer: {
+    port: 6500,
+  },
+};
